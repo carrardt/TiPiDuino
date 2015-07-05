0e6332c186a254f26a2a0ba46eea04ae9712488c
Thierry Carrard <thierry.carrard@orange.fr>
2015-07-05 20:49:12 +0200

    framebuffer readback is now done through plugins

---
+++ fleye/plugins/glReadBack.c
@@ -0,0 +1,13 @@
+#include <GLES2/gl2.h>
+#include <stdio.h>
+#include "../cpu_tracking.h"
+
+void glReadBack_setup()
+{
+	printf("glReadBack plugin ready\n");
+}
+
+void glReadBack_run(CPU_TRACKING_STATE * state)
+{
+	glReadPixels(0, 0, state->width, state->height,GL_RGBA,GL_UNSIGNED_BYTE, state->image);
+}
