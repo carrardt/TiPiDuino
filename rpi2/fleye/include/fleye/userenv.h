959f3cca2b6b9847763179558d093f8524891560
Thierry Carrard <thierry.carrard@orange.fr>
2015-12-12 18:17:55 +0100

    to be removed

--- rpi2/fleye/include/fleye/userenv.h
+++ rpi2/fleye/include/fleye/userenv.h
@@ -1,20 +1,12 @@
 #ifndef __fleye_UserEnv_H_
 #define __fleye_UserEnv_H_
 
-#include "fleye/config.h"
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct UserEnv
-{
-        int n_opt_values;
-        char opt_values[MAX_OPT_VALUES][2][32];
-};
-
-extern const char* fleye_optional_value(struct UserEnv* env, const char* key);
-extern int fleye_add_optional_value(struct UserEnv* env, const char* key, const char* value);
 
 #ifdef __cplusplus
 }
