264b8071187cfb4074dcb5d0788c1613e9dad19c
Thierry Carrard <thierry.carrard@orange.fr>
2016-03-10 21:46:26 +0100

    changed to spectrum output

--- host/fft_fp16.cpp
+++ host/fft_fp16.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <cmath>
+#include <cstdint>
+
+//#define FAKE_FP16 1
+
+/*
+ * FLOAT RANGE = approximately [-8;8] ( with input signal in [0;1] )
+ * so 1 bit for sign, 5 bits for integer and 10 bits for fraction is enough.
+ * could be 4 bits for integer and 11 bits for fraction
+ */
 
 struct FP16
 {
@@ -10,38 +19,50 @@ struct FP16
 	static constexpr float from_float = static_cast<float>( 1<<fbits );
 
 	inline FP16() {}
-	inline FP16(float x) { fromFloat(x) }
+	inline FP16(float x) { fromFloat(x); }
 	inline FP16(const FP16& x) : fp(x.fp) {}
 
+	inline operator float () const { return toFloat(); }
+
 #ifdef FAKE_FP16
-	inline void fromFloat(float x) { fp = x; }
+	inline void _fromFloat(float x) { fp = x; }
 	inline float toFloat() const { return fp; }
 	inline int floor() const { return std::floor(fp); }
 #else
-	inline void fromFloat(float x) { fp = static_cast<int>(x*from_float); }
+	inline void _fromFloat(float x) { fp = static_cast<int>(x*from_float); }
 	inline float toFloat() const { return fp * to_float; }
 	inline int floor() const { return fp>>fbits; }
+	inline void setFP(int16_t v) {fp=v;}
 #endif
 
-	inline updateRange()
+	inline void fromFloat(float x)
 	{
-		float x=toFloat()
-		float x=toFloat();;
-		if( toFloat() )
+		_fromFloat(x);
+		updateRange();
+	}
+
+	inline void updateRange()
+	{
+		float x = toFloat();
+		if( x < rmin ) rmin = x;
+		if( x > rmax ) rmax = x;
 	}
 
 	inline FP16& operator = ( FP16 x ) { fp = x.fp; return *this; }
 
-	inline FP16& operator += ( FP16 x ) { fromFloat( toFloat() + x.toFloat() ); return *this; }
+	inline FP16& operator += ( FP16 x ) { fp += x.fp; return *this; }
 	inline FP16& operator += ( float x ) { fromFloat( toFloat() + x ); return *this; }
 
-	inline FP16 operator + ( FP16 x ) const { return FP16(toFloat()+x.toFloat()); }
+	inline FP16& operator *= ( FP16 x ) { int32_t t=fp; t*=x.fp; fp=t>>fbits; return *this; }
+	inline FP16& operator *= ( float x ) { fromFloat( toFloat() * x ); return *this; }
+
+	inline FP16 operator + ( FP16 x ) const { FP16 r; r.setFP(fp+x.fp); return r; }
 	inline FP16 operator + ( float x ) const { return FP16(toFloat()+x); }
 
-	inline FP16 operator - ( FP16 x ) const { return FP16(toFloat()-x.toFloat()); }
+	inline FP16 operator - ( FP16 x ) const { FP16 r; r.setFP(fp-x.fp); return r; }
 	inline FP16 operator - ( float x ) const { return FP16(toFloat()-x); }
 
-	inline FP16 operator * ( FP16 x ) const { return FP16(toFloat()*x.toFloat()); }
+	inline FP16 operator * ( FP16 x ) const { int32_t t=fp; t*=x.fp; FP16 r; r.setFP(t>>fbits); return r; }
 	inline FP16 operator * ( float x ) const { return FP16(toFloat()*x); }
 
 #ifdef FAKE_FP16
@@ -53,13 +74,16 @@ struct FP16
 	static float rmin, rmax;
 };
 
-#define SIN_2PI_16 0.38268343236508978f
-#define SIN_4PI_16 0.707106781186547460f
-#define SIN_6PI_16 0.923879532511286740f
-#define C_P_S_2PI_16 1.30656296487637660f
-#define C_M_S_2PI_16 0.54119610014619690f
-#define C_P_S_6PI_16 1.3065629648763766f
-#define C_M_S_6PI_16 -0.54119610014619690f
+float FP16::rmin=0.0f;
+float FP16::rmax=0.0f;
+
+#define SIN_2PI_16 FP16(0.38268343236508978f)
+#define SIN_4PI_16 FP16(0.707106781186547460f)
+#define SIN_6PI_16 FP16(0.923879532511286740f)
+#define C_P_S_2PI_16 FP16(1.30656296487637660f)
+#define C_M_S_2PI_16 FP16(0.54119610014619690f)
+#define C_P_S_6PI_16 FP16(1.3065629648763766f)
+#define C_M_S_6PI_16 FP16(-0.54119610014619690f)
 
 /* INPUT: float input[16], float output[16] */
 /* OUTPUT: none */
@@ -97,9 +121,15 @@ struct FP16
 /* for n from 0 to 7.  To find F[4k+1] we take the 4 point Complex FFT of */
 /* exp(-2*pi*j*n/16)*{x[n] - x[n+8] + j(x[n+12]-x[n+4])} for n from 0 to 3.*/
 
-void R16SRFFT(float input[16],float output[16] ) {
-  float temp, out0, out1, out2, out3, out4, out5, out6, out7, out8;
-  float out9,out10,out11,out12,out13,out14,out15;
+static float mag(FP16 re, FP16 im)
+{
+	return	sqrtf(re*re+im*im);
+}
+
+void R16SRFFT(float input[16],float spectrum[8] ) {
+  FP16 temp, out0, out1, out2, out3, out4, out5, out6, out7, out8;
+  FP16 out9,out10,out11,out12,out13,out14,out15;
+  FP16 output[16];
 
   out0=input[0]+input[8]; /* output[0 through 7] is the data that we */
   out1=input[1]+input[9]; /* take the 8 point real FFT of. */
@@ -209,35 +239,47 @@ void R16SRFFT(float input[16],float output[16] ) {
   output[6]=out4-out5; /*Real Part of X[5] */
   output[10]=-out7-out6; /* Imag Part of X[7] */
 
+  spectrum[0] = mag( output[1] , output[9] );
+  spectrum[1] = mag( output[2] , output[10] );
+  spectrum[2] = mag( output[3] , output[11] );
+  spectrum[3] = mag( output[4] , output[12] );
+  spectrum[4] = mag( output[5] , output[13] );
+  spectrum[5] = mag( output[6] , output[14] );
+  spectrum[6] = mag( output[7] , output[15] );
+  spectrum[7] = mag( output[8] , 0.0f );	
 }
 
-void main() {
+
+int main() {
   float data[16];
   float output[16];
   float zero=0;
 
-  printf("\ntype 16 point input vector\n");
   scanf("%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",&data[0],&data[1],&data[2],&data[3],&data[4],&data[5],&data[6],&data[7],&data[8],&data[9],&data[10],&data[11],&data[12],&data[13],&data[14],&data[15]);
-  
+  printf("input: %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14],data[15]);
+
+#ifdef FAKE_FP16
+  FP16::rmin = FP16::rmax = data[0];
+#endif
+
   R16SRFFT(data,output);
   printf("\nresult is:\n");
-  printf("k,\t\tReal Part\t\tImaginary Part\n");
-    printf("0\t\t%.9f\t\t%.9f\n",output[0],zero);
-  printf("1\t\t%.9f\t\t%.9f\n",output[1],output[9]);
-  printf("2\t\t%.9f\t\t%.9f\n",output[2],output[10]);
-  printf("3\t\t%.9f\t\t%.9f\n",output[3],output[11]);
-  printf("4\t\t%.9f\t\t%.9f\n",output[4],output[12]);
-  printf("5\t\t%.9f\t\t%.9f\n",output[5],output[13]);
-  printf("6\t\t%.9f\t\t%.9f\n",output[6],output[14]);
-  printf("7\t\t%.9f\t\t%.9f\n",output[7],output[15]);
-  printf("8\t\t%.9f\t\t%.9f\n",output[8],zero);
-  printf("9\t\t%.9f\t\t%.9f\n",output[7],-output[15]);
-  printf("10\t\t%.9f\t\t%.9f\n",output[6],-output[14]);
-  printf("11\t\t%.9f\t\t%.9f\n",output[5],-output[13]);
-  printf("12\t\t%.9f\t\t%.9f\n",output[4],-output[12]);
-  printf("13\t\t%.9f\t\t%.9f\n",output[3],-output[11]);
-  printf("14\t\t%.9f\t\t%.9f\n",output[2],-output[9]);
-  printf("15\t\t%.9f\t\t%.9f\n",output[1],-output[8]);
+  printf("k,\t\tReal Part\t\tImaginary Part\t\tPower\n");
+  //print_result(0,output[0],zero);
+  print_result(1,output[1],output[9]);
+  print_result(2,output[2],output[10]);
+  print_result(3,output[3],output[11]);
+  print_result(4,output[4],output[12]);
+  print_result(5,output[5],output[13]);
+  print_result(6,output[6],output[14]);
+  print_result(7,output[7],output[15]);
+  print_result(8,output[8],zero);
+
+#ifdef FAKE_FP16
+  printf("float range = [%g;%g]\n",FP16::rmin,FP16::rmax);
+#endif
+
+	return 0;
 }
 
 
