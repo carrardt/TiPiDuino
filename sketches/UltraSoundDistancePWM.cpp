e9dd57df4b95679780d17a0d016b469797205ab1
carrardt <thierry.carrard@orange.fr>
2016-03-24 21:25:16 +0100

    compiles but not tested

--- sketches/UltraSoundDistancePWM.cpp
+++ sketches/UltraSoundDistancePWM.cpp
@@ -10,9 +10,9 @@ using namespace avrtl;
 
 #define SMOOTHING 1 // 0,1,2 or 3 level of input value smoothing
 
-auto trigger = StaticPin<TRIGGER_PIN>();
-auto echo = StaticPin<ECHO_PIN>();
-auto pwm = StaticPin<PWM_PIN>();
+static auto trigger = StaticPin<TRIGGER_PIN>();
+static auto echo = StaticPin<ECHO_PIN>();
+static auto pwm = StaticPin<PWM_PIN>();
 
 /*
 #define INIT_CLOCK_COUNTER() 	uint8_t _t=TCNT0,_t0=_t; int32_t _m=-_t0
@@ -25,95 +25,43 @@ auto pwm = StaticPin<PWM_PIN>();
 
 struct TimeScheduler
 {
-	uint8_t m_t;
 	int16_t m_wallclock;
+	uint8_t m_t;
 	
 	inline void start()
 	{
+		m_wallclock = 0;
 		m_t = TCNT0;
-		m_wallclock = -((int16_t)m_t);
 	}
-	
-	inline void update()
-	{
-		uint8_t t2 = TCNT0;
-		if(t2<m_t) m_wallclock += 256;
-		m_t = t2;
-	}
-	
+		
 	inline int16_t wallclock()
 	{
-		update();
-		return m_wallclock + m_t;
+		uint8_t t2 = TCNT0;
+		m_wallclock += (uint8_t)( t2 - m_t );
+		m_t = t2;
+		return m_wallclock;
 	}
-
-	inline void shift(int16_t s)
-	{
-		m_wallclock -= s;
-	}
-};
-
-template<typename FuncT>
-struct TSConstantTimeAction
-{
-	inline TSConstantTimeAction(FuncT _f,int16_t _t) : f(_f) , t(_t) {}
-	inline int16_t run(TimeScheduler& ts)
+	
+	template<typename FuncT>
+	inline void exec( int16_t t, FuncT f )
 	{
 		f();
-		while( ts.wallclock() < t );
-		return t;
+		while( wallclock() < t );
+		m_wallclock -= t;				
 	}
-	FuncT f;
-	int16_t t;
-};
 
-template<typename FuncT>
-struct TSConstantTimeLoop
-{
-	inline TSConstantTimeLoop(FuncT _f,int16_t _t) : f(_f) , t(_t) {}
-	inline int16_t run(TimeScheduler& ts)
+	template<typename FuncT>
+	inline void loop( int16_t t, FuncT f )
 	{
-		while( ts.wallclock() < t ) { f(); }
-		return t;
+		t = avrtl::microsecondsToTicks(t);
+		int16_t w;
+		while( (w=wallclock()) < t ) { f( avrtl::ticksToMicroseconds(w) ); }
+		m_wallclock -= t;				
 	}
-	FuncT f;
-	int16_t t;
+	
 };
 
-template<typename FuncT>
-struct TSMaxTimeCondLoop
-{
-	inline TSMaxTimeCondLoop(FuncT _f,int16_t _t) : f(_f) , t(_t) {}
-	inline int16_t run(TimeScheduler& ts)
-	{
-		bool cond = true;
-		while( (ts.wallclock() < t) && cond )
-		{ 
-			cond = f();
-		}
-		return 0;
-	}
-	FuncT f;
-	int16_t t;
-};
-
-template<typename Func1T, typename Func2T>
-struct TSConstantTimeSequence
-{
-	inline TSConstantTimeSequence(Func1T _f1, Func2T _f2, int16_t _t) : f1(_f1), f2(_f2) , t(_t) {}
-	inline int16_t run(TimeScheduler& ts)
-	{
-		f1();
-		ts.update();
-		f2();
-		while( ts.wallclock() < t );
-		return t;
-	}
-	Func1T f1;
-	Func2T f2;
-	int16_t t;
-};
-
+static TimeScheduler ts;
 
 void setup()
 {
@@ -124,87 +72,61 @@ void setup()
 
 void loop()
 {
-	constexpr uint16_t CycleTicks =  microsecondsToTicks(10050);
-	constexpr uint16_t TriggerTicks =  microsecondsToTicks(10);
-	constexpr uint16_t EchoTimeOutTicks =  microsecondsToTicks(2500);
-	constexpr uint16_t EchoGapMinTicks =  microsecondsToTicks(1000);
-	constexpr uint16_t EchoLengthMinTicks =  microsecondsToTicks(300);
-	constexpr uint16_t EchoLengthMaxTicks =  microsecondsToTicks(2200);
-	
 	uint16_t echo_length = 0;
 	uint16_t echo_gap = 0;
-	uint16_t value = microsecondsToTicks(768);
-	uint16_t targetvalue = microsecondsToTicks(768);
+	uint16_t value = 1024;
+	uint16_t targetValue = 1024;
 	uint8_t counter = 0;
 	
-	uint16_t HighPeriodTicks = value;
-	uint16_t LowPeriodTicks = 0;
-	uint16_t nextSwitchTime = 0;		
-	uint16_t wallClock = 0;
-	uint16_t tmpClock = 0;
-
 	SCOPED_SIGNAL_PROCESSING;
-		
-	INIT_CLOCK_COUNTER16();		
-	pwm.Set(true);
-	while( HighPeriodTicks < CycleTicks )
+	ts.start();
+
+	while( true )
 	{
-		// shift timer back so that 16bits wallclock is always enough
-		REWIND_CLOCK_COUNTER(LowPeriodTicks);
-		
-		do {
-			UPDATE_CLOCK_COUNTER();
-			wallClock = CLOCK_ELAPSED();
-		} while( wallClock < HighPeriodTicks );
+		ts.exec( value,
+				[&echo_gap, &echo_length]()
+				{ 
+					pwm=HIGH;
+					echo_gap = 0;
+					echo_length = 0;
+				} );
+				
+		ts.exec( 40 ,
+				[counter]()
+				{
+					pwm=LOW;
+				} );
 
-		// as soon as we detect high time is elapsed, set to low
-		pwm.Set(false);
+		ts.exec( 10 ,
+				[counter]()
+				{
+					trigger = (counter==0);
+				} );
+				
+		ts.exec( 50,
+				[]()
+				{
+					trigger=LOW;
+				} );
 		
-		// keep timer in sync
-		UPDATE_CLOCK_COUNTER();
-		
-		// shift timer back so that 16bits wallclock is always enough
-		REWIND_CLOCK_COUNTER(HighPeriodTicks);
-		
-		// update timings
-		LowPeriodTicks = CycleTicks - HighPeriodTicks;
+		ts.loop( 5000,
+				[&echo_gap,&echo_length](int16_t t) {
+					bool e = echo.Get();
+					if( echo_gap==0 && e ) { echo_gap = t; }
+					if( echo_gap!=0 && !e ) { echo_length = t - echo_gap; }
+				} );
 
-		/**** BEGIN Ultra-Sound MEASURE *****/
-		UPDATE_CLOCK_COUNTER();
-		if( counter == 6 )
-		{
-			trigger = true;
-			UPDATE_CLOCK_COUNTER();
-			tmpClock = CLOCK_ELAPSED();
-			while( (CLOCK_ELAPSED()-tmpClock) < TriggerTicks ) { UPDATE_CLOCK_COUNTER(); }
-			trigger = false;
-			/*tmpClock = CLOCK_ELAPSED();
-			while( !echo.Get() && (CLOCK_ELAPSED()-tmpClock) < EchoTimeOutTicks ) { UPDATE_CLOCK_COUNTER(); }
-			echo_gap = CLOCK_ELAPSED() - tmpClock;
-			while( echo.Get() && (CLOCK_ELAPSED()-tmpClock) < (2*EchoTimeOutTicks) ) { UPDATE_CLOCK_COUNTER(); }
-			echo_length = CLOCK_ELAPSED() - tmpClock - echo_gap;
-			if( echo_gap>EchoGapMinTicks && echo_length>EchoLengthMinTicks && echo_length<EchoLengthMaxTicks )
-			{
-				targetvalue = echo_length;
-			}*/
-			counter = 0;
-		}
-		else { ++ counter; }
-		//UPDATE_CLOCK_COUNTER();
-		//value = ( (value<<SMOOTHING) - value + targetvalue ) >> SMOOTHING;
-		/**** END US MEASURE *****/
+		ts.exec( 10000-(value+5100),
+				[&value,&targetValue,&counter,echo_gap,echo_length](){
+					if( echo_gap>1024 && echo_length>320 )
+					{
+						targetValue = microsecondsToTicks( echo_length/2 + 350 );
+					}
+					value = ( (value<<SMOOTHING)-value+targetValue ) >> SMOOTHING ;
+					++ counter;
+					if( counter == 6 ) counter = 0;
+				} );
 		
-		UPDATE_CLOCK_COUNTER();
-		HighPeriodTicks = value;
-		bool highState = (HighPeriodTicks>0);
-		
-		do {
-			UPDATE_CLOCK_COUNTER();
-			wallClock = CLOCK_ELAPSED();
-		} while( wallClock<LowPeriodTicks );
-
-		// as soon as we detect low time is elapsed, set to high
-		pwm.Set(highState);
 	}
 }
 
