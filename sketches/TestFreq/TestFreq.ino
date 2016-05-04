7b29b110acc4cd5844af894f8f2a3198dde089b9
carrardt <thierry.carrard@orange.fr>
2016-05-04 15:26:37 +0200

    translated comments

--- sketches/TestFreq/TestFreq.ino
+++ sketches/TestFreq/TestFreq.ino
@@ -1,9 +1,9 @@
 /*
  * Test de fréquence pour l'ATtiny85 (ou autre)
- * 1. Graver la séquence d'initialisation avec la config voulue (i.e. 8Mhz internal clock, etc.)
- * 2. téléverser le programme
- * 3. mesurer la durée d'un créneau à l'oscilo. par exemple T = 0.0005 sec (500uS)
- * 4. Fréquence d'horloge = 8192 / T
+ * 1. upload bootlader corresponding to desired configuration (i.e. 8Mhz internal clock, etc.)
+ * 2. upload the sketch
+ * 3. measure the time T of a high pulse with osciloscope (in seconds)
+ * 4. Clock frequency = 8192 / T. check if it matches your expectations
  */
 #define CLK_PIN 0
 #define LED_PIN 1
@@ -25,5 +25,4 @@ void loop() {
   counter += tick^tock;
   digitalWrite(LED_PIN, (counter>>10) & 0x01  );
   tick = tock;
-  //PORTB = ( (counter>>10) & 0x02 ) | tock;
 }
