c4b4afb15960a2eb690e88f5770a190225e89a16
Thierry Carrard <thierry.carrard@orange.fr>
2016-12-23 18:46:47 +0100

    fixed version number (1.5) and added digital read to linkuino_tool

--- host/Linkuino/LinkuinoClient.h
+++ host/Linkuino/LinkuinoClient.h
@@ -35,6 +35,7 @@ struct LinkuinoClient
 	void setDigitalOutput(uint8_t mask);
 
 	uint16_t requestAnalogRead(uint8_t channel);
+	int16_t requestDigitalRead();
 
 	//! Note: auto send on invocation
 	void forwardMessage( uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3 );
