e13aba95daf3ddfb6810659da0d64ec20cc3498d
carrardt <thierry.carrard@orange.fr>
2017-07-02 21:21:39 +0200

    rewrite of the CNC job optimizer, partially supporting 3D. support generic operations that can be combined to acheive more complex processing

---
+++ pcb/python/GCode.py
@@ -0,0 +1,248 @@
+from Geometry import *
+
+class GCode:
+	def __init__(self):
+		self.segments=[]
+		self.bbox=AABB()
+		self.travelZ = 1.0
+		
+	def readFromFile(self,filename):
+		self.readFromStream(open(filename,"r"))
+		
+	def readFromStream(self,fin):
+		commands = fin.read().split('\n')
+		x = 0.0
+		y = 0.0
+		z = 0.0
+		specialCommands=[]
+		segment=Segment()
+		S = segment.getSpindle()
+		F = segment.getSpeed()
+		for l in commands:
+			if l.startswith("G0"):
+				c = l[:3]
+				if l.find('X')>=0: x = float(l.replace('Y','X').replace('Z','X').split('X')[1])
+				if l.find('Y')>=0: y = float(l.replace('Z','Y').split('Y')[1])
+				if l.find('Z')>=0: z = float(l.split('Z')[1])
+				segment.addNode( Point(x,y,z) )
+			elif len(l)>0:
+				if segment.numberOfNodes()>0:
+					segment.setSpindle(S)
+					segment.setSpeed(F)
+					self.segments.append(segment)
+					segment = Segment()
+				cmds = l.split(' ')
+				for c in cmds:
+					if len(c)>0:
+						if c.startswith("F"):
+							F = float(c[1:])
+						if c.startswith("S"):
+							S = float(c[1:])
+		if not segment.empty():
+			self.segments.append(segment)
+		self.updateBounds()
+		self.setTravelZ( self.bbox.zMax )
+		
+	def header(self):
+		return "G21\nG90\nG94\nG00 Z%g\nG00 X0Y0\n" % self.travelZ
+
+	def write(self,stream):
+		stream.write(self.header())
+		for s in self.segments:
+			s.write(stream)
+		stream.write("M05\n")
+
+	def addTravelOffset(self,TravelPlane,Code):
+		for s in self.segments:
+			if s.numberOfNodes()>0:
+				Ps = TravelPlane.project( s.nodes[0] )
+				Ps.c = Code
+				Pe = TravelPlane.project( s.nodes[-1] )
+				Pe.code = Code
+				s.nodes = [Ps]+s.nodes+[Pe]
+
+	def mergeSegments(self):
+		if self.numberOfSegments()<2:
+			return
+		prevSeg = self.segments[0]
+		segList=[]
+		for s in self.segments[1:]:
+			if s.getSpindle()==prevSeg.getSpindle() and s.getSpeed()==prevSeg.getSpeed():
+				prevSeg.nodes += s.nodes
+			else:
+				segList.append(prevSeg)
+				prevSeg = s
+		segList.append(prevSeg)
+		self.segments = segList
+
+	def updateBounds(self):
+		self.bbox=AABB()
+		for s in self.segments:
+			self.bbox = self.bbox.union(s.bbox)
+
+	def gapDistance(self):
+		if len(self.segments)==0:
+			return 0.0
+		s=self.segments[0]
+		l=0.0
+		for ns in self.segments[1:]:
+			l+=s.gapDistance(ns)
+			s=ns
+		return l
+
+	def totalLength(self):
+		l = 0.0
+		for s in self.segments:
+			l += s.length()
+		return l
+		
+	def numberOfNodes(self):
+		n=0
+		for s in self.segments:
+			n+=s.numberOfNodes()
+		return n
+
+	def numberOfSegments(self):
+		return len(self.segments)
+
+	def setTravelZ(self,z):
+		self.travelZ = z
+		
+	def zCutOff(self,zcut=0.0):
+		newSegments = []
+		for s in self.segments:
+			l = s.zCutOff(zcut)
+			newSegments += l
+		self.segments = newSegments
+		self.updateBounds()
+		
+	def numberOfLoops(self):
+		n = 0
+		for s in self.segments:
+			if s.isLoop():
+				n+=1
+		return n
+
+	def subdivide(self,axis='?',threshold=0.2):
+		if self.bbox.xyArea()==0.0:
+			return (self,None)
+		bb1 = self.bbox.copy() # aka left part
+		bb2 = self.bbox.copy() # aka right part
+		cutPlane = None
+		(W,H,Z) = self.bbox.size()
+		(xc,yc,zc) = self.bbox.center()
+		if (W>=H and W>=Z and axis.lower()=='?') or axis.lower()=='x':
+			bb1.xMax = xc
+			bb2.xMin = xc
+			cutPlane = Plane(1.0,0.0,0.0,-xc)
+			axis='X'
+		elif (H>=Z and axis.lower()=='?') or axis.lower()=='y':
+			bb1.yMax = yc
+			bb2.yMin = yc
+			cutPlane = Plane(0.0,1.0,0.0,-yc)
+			axis='Y'
+		else:
+			bb1.zMax = zc
+			bb2.zMin = zc
+			cutPlane = Plane(0.0,0.0,1.0,-zc)
+			axis='Z'
+			raise BaseException("Z axis not supported yet for subdivide operations")
+		#print("Cut axis = %s"%axis)
+		if bb1.xyArea()+bb2.xyArea() != self.bbox.xyArea():
+			raise BaseException("bounding box splitting error")
+		#else:
+		#	print("split BBox:\n%s\n%s\n%s"%(self.bbox,bb1,bb2))
+		s1 = []
+		s2 = []
+		nCuts = 0
+		maxOverlapRatio = 1.0 + threshold
+		for s in self.segments:
+			sbb = s.bounds()
+			sa = sbb.xyArea()
+			assignToLeft = bb1.containsBounds(sbb)
+			assignToRight = bb2.containsBounds(sbb)
+			r1=0.0
+			r2=0.0
+			if not assignToLeft and not assignToRight:
+				r1 = sbb.union(bb1).xyArea() / bb1.xyArea()
+				r2 = sbb.union(bb2).xyArea() / bb2.xyArea()
+				if r1<=r2 and r1<=maxOverlapRatio:
+					assignToLeft = True
+				elif r2<r1 and r2<=maxOverlapRatio:
+					assignToRight = True
+			if assignToLeft:
+				s1.append(s)
+			elif assignToRight:
+				s2.append(s)
+			else:
+				#print("segment cut : r1=%g, r2=%g, sbb=%s"%(r1,r2,str(sbb)))
+				( leftList , rightList ) = s.planeCut( cutPlane )
+				s1 += leftList
+				s2 += rightList
+				nCuts += 1
+		leftGCode = GCode()
+		leftGCode.bbox = bb1
+		leftGCode.segments = s1
+		leftGCode.updateBounds()
+		leftGCode.travelZ = self.travelZ
+		rightGCode = GCode()
+		rightGCode.bbox = bb2
+		rightGCode.segments = s2
+		rightGCode.updateBounds()
+		rightGCode.travelZ = self.travelZ
+		print("split end : %d cuts, overlap=%g," % (nCuts,leftGCode.bbox.intersection(rightGCode.bbox).xyArea()/self.bbox.xyArea()) )
+		return ( leftGCode , rightGCode )
+	
+	def merge(self,gc):
+		self.travelZ = max(self.travelZ,gc.travelZ)
+		self.segments += gc.segments
+		self.bbox = self.bbox.union( gc.bbox )
+
+	def findNearestSegment(self,p):
+		ns = self.numberOfSegments()
+		if ns==0:
+			return None
+		minSI = 0
+		(minNodeI,minNodeD) = self.segments[0].findNearestNode(p)
+		for si in range(1,ns):
+			s = self.segments[si]
+			(nodei,noded) = s.findNearestNode(p)
+			if noded < minNodeD:
+				(minSI,minNodeI,minNodeD) = (si,nodei,noded)
+		return (minSI,minNodeI,minNodeD)
+
+	def optimizeSegments(self,p):
+		ns = self.numberOfSegments()
+		if ns==0: return p
+		if ns==1: return self.segments[0].lastNode()
+		segList = []
+		if p==None:
+			p = Point(0.0,0.0,self.travelZ)
+		while len(self.segments)>0:
+			(si,nodei,noded) = self.findNearestSegment(p)
+			s = self.segments.pop(si)
+			s.rotateNodes(nodei)
+			p = s.lastNode()
+			segList.append( s )
+		self.segments = segList
+		return p
+		
+	def printStats(self):
+		print("Number of segments = %d"%len(self.segments))
+		print("Number of nodes = %d"%self.numberOfNodes())
+		print("Loops = %d"%self.numberOfLoops())
+		print("Total length = %g"%self.totalLength())
+		print("Total gap distance = %g"%self.gapDistance())
+		print("Travel Z = %g"%self.travelZ)
+		print("Bounds = %s"%str(self.bbox))
+
+	def print(self):
+		print("Number of segments = %d"%len(self.segments))
+		print("Total length = %g"%self.totalLength())
+		print("Total gap distance = %g"%self.gapDistance())
+		print("Travel Z = %g"%self.travelZ)
+		print("Bounds = %s"%str(self.bbox))
+		for (i,s) in enumerate(self.segments):
+			print("\nSegment %d"%i)
+			s.print()
+		
--- pcb/python/ncOptimizer.py
+++ pcb/python/ncOptimizer.py
@@ -1,306 +1,96 @@
 #!/c/Users/thier/AppData/Local/Programs/Python/Python35-32/python.exe
 
-import os
-import sys
-import string
-import math
-import random
-
-OPTIMIZATION_ITERATION_COUNT=1000
-OPTIMIZATION_RANDOM_SWAPS=13
-
-class AABB:
-	def __init__(self):
-		self.xMin=1.e6
-		self.xMax=-1.e6
-		self.yMin=1.e6
-		self.yMax=-1.e6
-		self.zMin=1.e6
-		self.zMax=-1.e6
-	def union(self, o):
-		self.xMin = min( self.xMin , o.xMin )
-		self.yMin = min( self.yMin , o.yMin )
-		self.zMin = min( self.zMin , o.zMin )
-		self.xMax = max( self.xMax , o.xMax )
-		self.yMax = max( self.yMax , o.yMax )
-		self.zMax = max( self.zMax , o.zMax )
-
-class SegmentNode:
-	def __init__(self):
-		self.code="G01"
-		self.x=0.0
-		self.y=0.0
-		self.z=0.0
-
-class Segment:
-	def __init__(self):
-		self.nodes=[]
-
-class GCode:
-	def __init__(self):
-		self.xMin=1.e6
-		self.xMax=-1.e6
-		self.yMin=1.e6
-		self.yMax=-1.e6
-		self.zMin=1.e6
-		self.zMax=-1.e6
-		self.header=[]
-		self.body=[]
-		self.footer=[]
-	def writeHeader(self,stream):
-		for cl in self.header:
-			for c in cl:
-				stream.write( " ".join(c) + "\n" )
-	def writeFooter(self,stream,terminate=False):
-		for cl in self.footer:
-			for c in cl:
-				cmd = " ".join(c)
-				if cmd!="M05" or terminate: stream.write( cmd + "\n" )
-	def writeBody(self,stream,tf):
-		for cl in self.body:
-			(x,y,z) = tf( cl[0][1] )
-			z = self.zMax
-			stream.write("G00 Z%.4f\n"%self.zMax)
-			stream.write("G00 X%.4fY%.4f\n"%(x,y))
-			for c in cl:
-				(nx,ny,nz) = tf( c[1] )
-				coordStr = ""
-				if nx!=x: coordStr += "X%.4f" % nx
-				if ny!=y: coordStr += "Y%.4f" % ny
-				if nz!=z: coordStr += "Z%.4f" % nz
-				(x,y,z) = (nx,ny,nz)
-				if coordStr!="": stream.write(c[0]+" "+coordStr+"\n")
-		stream.write("G00 Z%.4f\n"%self.zMax)
-	def write(self,stream,terminate=False, tf=lambda p:p):
-		self.writeHeader(stream)
-		self.writeBody(stream,tf)
-		self.writeFooter(stream,terminate)
-	def updateXYBounds(self):
-		self.xMin=1.e6
-		self.xMax=-1.e6
-		self.yMin=1.e6
-		self.yMax=-1.e6
-		for cl in self.body:
-			for c in cl:
-				(x,y,z) = c[1]
-				if x<self.xMin: self.xMin=x
-				if x>self.xMax: self.xMax=x
-				if y<self.yMin: self.yMin=y
-				if y>self.yMax: self.yMax=y
-
-def readGCodeFromFile(filename):
-	gcode = GCode()
-	commands = open(filename,"r").read().split('\n')
-	x = 0.0
-	y = 0.0
-	z = 0.0
-	specialSegment = []
-	lineSegment = []
-	block = []
-	zMin = 1.e6
-	zMax = -1.e6
-	for l in commands:
-		if l.startswith("G0"):
-			c = l[:3]
-			if len(specialSegment)>0:
-				block.append( specialSegment )
-				specialSegment = []
-			if l.find('X')>=0: x = float(l.replace('Y','X').replace('Z','X').split('X')[1])
-			if l.find('Y')>=0: y = float(l.replace('Z','Y').split('Y')[1])
-			if l.find('Z')>=0: z = float(l.split('Z')[1])
-			if z<=0: lineSegment.append( (c,(x,y,z)) )
-			else:
-				if z>zMax: zMax = z
-				if z<zMin: zMin = z
-				if len(lineSegment)>0:
-					block.append( lineSegment )
-					lineSegment = []
-		elif len(l)>0:
-			if len(lineSegment)>0:
-				block.append( lineSegment )
-				lineSegment = []
-			specialSegment.append( l.split() )
-	if len(lineSegment)>0: block.append( lineSegment )
-	if len(specialSegment)>0: block.append( specialSegment )
-	header = []
-	body = []
-	footer = []
-	for segment in block:
-		if segment[0][0].startswith("G0"):
-			if len(footer)>0:
-				print("Special commands found between segments, giving up")
-				return (0,0,[],[],[])
-			body.append( segment )
-		elif len(body)==0:
-			header.append(segment)
-		else:
-			footer.append(segment)
-	gcode.zMin = zMin
-	gcode.zMax = zMax
-	gcode.header = header
-	gcode.body = body
-	gcode.footer = footer
-	return gcode
-
-def computeGapDistance( segmentList ):
-	if len(segmentList)==0:
-		return 0.0
-	(x,y,z) = (0.0,0.0,0.0)
-	started = False
-	l = 0.0
-	for segment in segmentList:
-		(sx,sy,sz) = segment[0][1]
-		if not started:
-			(x,y,z) = (sx,sy,sz)
-			started = True
-		x-=sx ; y-=sy
-		l += math.sqrt( x*x + y*y )
-		(x,y,z) = segment[-1][1]
-	return l
-
-def dist2( a , b ):
-	x=a[0]-b[0];
-	y=a[1]-b[1];
-	return x*x+y*y
+def main():
+	import argparse
+	import GCode
+	import Geometry
+	parser = argparse.ArgumentParser(description='GCode processor')
+	parser.add_argument("input", type=str, nargs='+', help="input CNC files to process")
+	parser.add_argument("-o", "--output", type=str, required=True, help="output CNC file")
+	parser.add_argument("-z", "--z-cut", dest='zCutOff', type=float, nargs='?', help="cut part of segments crossing this Z level", const=0.0)
+	parser.add_argument("-c", "--z-connect", dest='zConnect', const=True, default=False, action='store_const', help="reconnect segments, travelling at specified z level")
+	parser.add_argument("-s", "--subdivide", type=int, help="number of subdivision passes (split into 2^N parts)")
+	parser.add_argument("-O", "--optimize", dest='optimize', const=True, default=False, action='store_const', help="Optimize segments")
+	parser.add_argument("-v", "--verbose", help="material surface Z level", const=True, default=False, action='store_const')
+	parser.add_argument("-p", "--print", help="print result to console", const=True, default=False, action='store_const')
+	parser.add_argument("-m", "--multiple-output", dest='multipleOutput', help="generate multiple output files, one per segment", const=True, default=False, action='store_const')
+	args = parser.parse_args()
+	gcode = GCode.GCode()
 	
-def nearestNeighborsSort( segmentListIn , maxLength ):
-	if len(segmentListIn)==0: return []
-	segmentList = segmentListIn.copy()
-	l = []
-	op = segmentList[0][-1][1]
-	dMax = maxLength*maxLength
-	while len(segmentList) > 0:
-		iMin = -1
-		dMin = dMax
-		for (i,s) in enumerate(segmentList):
-			p = s[0][1]
-			d = dist2( op , p )
-			if d < dMin:
-				iMin = i
-				dMin = d
-		op = segmentList[iMin][-1][1]
-		l.append( segmentList.pop(iMin) )
-	return l
-
-def randomSwapList( inputList , N ):
-	l = inputList.copy()
-	for i in range(N):
-		i=random.randint(0,len(l)-1)
-		j=random.randint(0,len(l)-1)
-		a = l[i]
-		b = l[j]
-		l[j] = a
-		l[i] = b
-	return l
-
-def optimizeSegmentList( segmentListIn ):
-	segmentList = segmentListIn.copy()
-	cost = computeGapDistance( segmentList )
-	for i in range(OPTIMIZATION_ITERATION_COUNT):
-		optList = randomSwapList( segmentList, OPTIMIZATION_RANDOM_SWAPS )
-		optList = nearestNeighborsSort(optList,1.e6)
-		optCost = computeGapDistance(optList)
-		if optCost<cost:
-			cost=optCost
-			segmentList = optList
-	return segmentList
-
-if "-h" in sys.argv:
-	print("""
-Usage: ncOptimizer [-transform sx sy ax ay] [-n] [-o] [-s] <output_file> <input_file1> <input_file2> ...
-Options:
-	-h : this help
-	-transform sx sy ax ay : multiply X'=X*sx+ax Y'=Y*sy+ay
-	-n : normalize origin, so that bounding box' min is 0,0
-	-o : optimize paths
-	-s : split blocks to separate files
-	""")
-	sys.exit(0)
-
-(SX,SY,AX,AY) = (1.,1.,0.,0.)
-if "-transform" in sys.argv:
-	i = sys.argv.index("-transform")
-	sys.argv.pop(i)
-	(SX,SY,AX,AY) = ( float(sys.argv[i]) , float(sys.argv[i+1]) , float(sys.argv[i+2]) , float(sys.argv[i+3]) )
-	sys.argv.pop(i)
-	sys.argv.pop(i)
-	sys.argv.pop(i)
-	sys.argv.pop(i)
-	print("Result will be horizontaly flipped")
-
-OffsetOrigin = False
-if "-n" in sys.argv:
-	OffsetOrigin = True
-	sys.argv.remove("-n")
-	print("Origin wil be normilized to 0,0")
-
-OptimizeWires = False
-if "-o" in sys.argv:
-	OptimizeWires = True
-	sys.argv.remove("-o")
-	print("Result will be optimized")
-
-SeparateFiles = False
-if "-s" in sys.argv:
-	SeparateFiles = True
-	sys.argv.remove("-s")
-	print("Result will be split into several files")
-
-print( "input : ", sys.argv[2:] )
-print( "output : ", sys.argv[1] )
-
-bbox = AABB()
-
-gcodeList = []
-for fileName in sys.argv[2:]:
-	print("Open %s ..."%fileName)
-	gcode = readGCodeFromFile( fileName )
-	if gcode.zMin != gcode.zMax:
-		print("variable travel Z, giving up")
-		sys.exit(1)
-	gcode.updateXYBounds()
-	bbox.union(gcode)
-	if len(gcodeList)>0 and gcode.header==gcodeList[-1].header and gcode.footer==gcodeList[-1].footer and gcode.zMax==gcodeList[-1].zMax:
-		print("compatible blocks, merging")
-		gcodeList[-1].body += gcode.body
+	for s in args.input:
+		print("read %s"%s)
+		gcode.readFromFile(s)
+		
+	if args.zCutOff!=None:
+		print("cut surface at Z=%g"%args.zCutOff)
+		gcode.zCutOff( args.zCutOff )
+	
+	startPoint = Geometry.Point(0.0,0.0,gcode.travelZ)
+	
+	if args.subdivide!=None:
+		gcodeList = [gcode]
+		N = int(args.subdivide)
+		while N>0:
+			L = 0.0
+			G = 0.0
+			for gc in gcodeList:
+				L += gc.totalLength()
+				G += gc.gapDistance()
+			print("subdivision, pass #%d, L=%g, G=%g" % (args.subdivide-N+1,L,G))
+			sl=[]
+			for gc in gcodeList:
+				(l,r) = gc.subdivide()
+				sl += [l,r]
+			gcodeList = sl
+			N -= 1
+		L = 0.0
+		G = 0.0
+		for gc in gcodeList:
+			L += gc.totalLength()		
+			G += gc.gapDistance()
+		print("Subdivision end, L=%g, G=%g"%(L,G))
+		if args.optimize:
+			print("optimize segments (before merge)")
+			p = startPoint
+			L = 0.0
+			G = 0.0
+			for gc in gcodeList:
+				p = gc.optimizeSegments(p)
+				L += gc.totalLength()
+				G += gc.gapDistance()
+			print("optimize end, L=%g, G=%g"%(L,G))
+		gcode = gcodeList[0]
+		for gc in gcodeList[1:]:
+			gcode.merge( gc )
+		gcodeList = None
 	else:
-		gcodeList.append( gcode )
+		if args.optimize:
+			(L1,G1) = (gcode.totalLength(),gcode.gapDistance())
+			print("Optimize start : L=%g, G=%g"%(L1,G1))
+			gcode.optimizeSegments(startPoint)
+			(L2,G2) = (gcode.totalLength(),gcode.gapDistance())
+			print("Optimize end : L=%g, G=%g"%(L2,G2))
+	
+	if args.zConnect:
+		print("reconnect segments")
+		TravelPlane = Geometry.Plane( 0.0,0.0,1.0,-gcode.travelZ )
+		gcode.addTravelOffset( TravelPlane, 'G00' )
+		gcode.mergeSegments()
 
+	if args.verbose:
+		gcode.printStats()
 
-if OffsetOrigin:
-	AX -= SX * bbox.xMin
-	AY -= SY * bbox.yMin
+	if args.print:
+		gcode.print()
 
-def transformCoord( p ):
-	(x,y,z) = p
-	return (x*SX+AX,y*SY+AY,z)
+	if args.multipleOutput:
+		N = gcode.numberOfSegments()
+		for i in range(N):
+			print("output segment #%d to %s.%d"%(i,args.output,i))
+			gcode.writeSingleSegment( open(args.output+'.'+str(i),'w') )
+	else:
+		print("output result to %s"%args.output)
+		gcode.write( open(args.output,'w') )
 
-output=None
-if not SeparateFiles:
-	output=open(sys.argv[1],"w")
-
-Fi=0
-for gcode in gcodeList:
-	if SeparateFiles:
-		output=open(sys.argv[1]+".%d"%Fi,"w")
-	gapLength = computeGapDistance(gcode.body)
-	print("Header:",gcode.header)
-	print("Footer:",gcode.footer)
-	print("before: %d segments, travel Z=%g, total gaps=%g"%(len(gcode.body),gcode.zMax,gapLength))
-	if OptimizeWires:
-		gcode.body = optimizeSegmentList( gcode.body )
-		gapLength = computeGapDistance(gcode.body)
-		print("after: %d segments, travel Z=%g, total gaps=%g"%(len(gcode.body),gcode.zMax,gapLength))
-	gcode.write(output,False,transformCoord)
-	if SeparateFiles:
-		output.write("M05\n")	
-		output.close()
-		output=None
-		Fi=Fi+1
-
-if not SeparateFiles:
-	output.write("M05\n")	
-	output.close()
-	output=None
-
-print("Done")
+if __name__=="__main__":
+	main()
