c568b03fd11194becb0cead529c77d5f5e966c8b
carrardt <thierry.carrard@orange.fr>
2018-02-11 19:57:52 +0100

    Added a sketch to produce fake numerical values to serial port

---
+++ sketches/SerialOutput/SerialOutput.ino
@@ -0,0 +1,21 @@
+
+int n = 0;
+
+void setup()
+{
+  Serial.begin(19200);
+}
+
+void loop()
+{
+	n = ( n + 1 ) % 1024;
+  float x = n*0.234f;
+  float y = n*0.874f;
+  float z = n*0.531f;
+  Serial.print(x);
+  Serial.print(" ");
+  Serial.print(y);
+  Serial.print(" ");
+  Serial.println(z);
+}
+
