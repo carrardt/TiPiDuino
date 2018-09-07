815b848ad1f440f1235a61bbda400a0793b625da
Thierry Carrard <thierry.carrard@cea.fr>
2018-09-07 19:19:59 +0200

    start rewrite project for direct screen driving

--- sketches/TimerRelaySelfShutdownSerial.cpp
+++ sketches/TimerRelaySelfShutdownSerial.cpp
@@ -1,6 +1,5 @@
 #include <AvrTL.h>
 #include <AvrTLPin.h>
-#include <SoftSerial.h>
 #include <BasicIO/ByteStream.h>
 #include <BasicIO/PrintStream.h>
 #include <TimeScheduler/TimeScheduler.h>
@@ -8,15 +7,14 @@
 #include <avr/interrupt.h>
 
 #define DEFAULT_TIMER_SECONDS 300
-#define SERIAL_SPEED 19200
 
-
-#define RELAY_PIN  		2
+#define RELAY_PIN  	2
 #define TIME_INC_PIN 	1
 #define TIME_DEC_PIN 	0
 #define POWER_OFF_PIN 	3
 #define SERIAL_OUT_PIN 	4
 
+// TODO: rotating address
 #define TIMER_EEPROM_ADDR0 ((uint8_t*)0x0010)
 #define TIMER_EEPROM_ADDR1 ((uint8_t*)0x0011)
 #define MAGICNUMBER_EEPROM_ADDR ((uint8_t*)0x0012)
@@ -33,16 +31,7 @@ static int32_t timerSeconds = DEFAULT_TIMER_SECONDS;
 static int32_t timerRemainingSeconds = DEFAULT_TIMER_SECONDS;
 static bool timerSecondsChanged = false;
 
-/*
-using SerialScheduler = TimeSchedulerT<AvrTimer0NoPrescaler>;
-static avrtl::NullPin rx;
-static auto tx = avrtl::StaticPin<SERIAL_OUT_PIN>();
-static auto rawSerialIO = make_softserial_hr<SERIAL_SPEED,SerialScheduler>(rx,tx);
-static ByteStreamAdapter<decltype(rawSerialIO),100000UL> serialIO = { rawSerialIO };
-static PrintStream cout;
-*/
-
-static TimeSchedulerT< AvrTimer0 , int32_t > ts;
+static TimeSchedulerT< AvrTimer1 , int32_t > ts;
 
 void setup()
 {
@@ -52,7 +41,6 @@ void setup()
 	relayPin.SetOutput();
 	relayPin.Set( true );
 
-/*
 	powerOffPin.SetInput();
 	powerOffPin.Set( HIGH ); // pullup
 
@@ -72,66 +60,44 @@ void setup()
 		eeprom_write_byte( MAGICNUMBER_EEPROM_ADDR , MAGICNUMBER_VALUE );
 	}
 	timerRemainingSeconds = timerSeconds;
-	*/
-	
-	/*
-	avrtl::DelayMicroseconds( 200000UL );
-	serialIO.m_rawIO.begin();
-	cout.begin( &serialIO );
-	cout<<"F_CPU="<<F_CPU<<endl;
-	cout<<"Ready"<<endl;
-	//rawSerialIO.ts.start();
-	*/
 	
 	ts.start();
 }
 
 // allows for key combinations
-static uint8_t getButtonCode()
+#define POWER_BUTTON_MASK	0X04
+#define UP_BUTTON_MASK		0X02
+#define DOWN_BUTTON_MASK	0X01
+
+static uint8_t gKeyState = 0;
+static uint8_t gKeyPressed = 0;
+static uint8_t gReadKey = 0;
+static void updateKeyPressed()
 {
 	uint8_t s = ! powerOffPin.Get();
 	uint8_t u = ! upPin.Get();
 	uint8_t d = ! downPin.Get();
 	uint8_t pc = s<<2 | u<<1 | d;
-	uint8_t press_code;
-	if( pc == 0 ) { return 0; }
-	
-	do
+	if( pc >= gKeyState )
 	{
-		press_code = pc;
-		s = ! powerOffPin.Get();
-		u = ! upPin.Get();
-		d = ! downPin.Get();
-		pc = s<<2 | u<<1 | d;
-	} while(  pc >= press_code );
-	
-	do
+		gKeyState = pc;
+	}
+	else 
 	{
-		s = ! powerOffPin.Get();
-		u = ! upPin.Get();
-		d = ! downPin.Get();
-		pc = s<<2 | u<<1 | d;
-	} while( pc != 0 );
-	
-	return press_code;
+		if( gKeyPressed == 0 ) { gKeyPressed = gKeyState; }
+		if( pc == 0 ) { gKeyState = 0; }
+	}
+}
+static uint8_t readKeyPressed()
+{
+	uint8_t k = gKeyPressed;
+	gKeyPressed = 0;
+	return k;
 }
 
-/*
-static uint8_t buffer[16];
 static void updateTimerLCD()
 {
-	uint8_t i = 0;
-	uint8_t* buf = buffer;
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = 'H'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = 'e'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = 'l'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = 'l'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = 'o'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = '\n'; });
-	rawSerialIO.ts.execFast( 1000, [&buf](){ *buf++ = '\0'; });
-	rawSerialIO.writeBufferFast( buffer );
 }
-*/
 
 static void powerOff()
 {
@@ -152,13 +118,13 @@ static void processButton(uint8_t bc)
 			timerRemainingSeconds = DEFAULT_TIMER_SECONDS;
 			timerSecondsChanged = true;
 			break;
-		case 1 : // more delay 
+		case 1 : // less delay 
 			if( timerSeconds > 9 ) { timerSeconds -= 5; }
 			if( timerRemainingSeconds > 5 ) timerRemainingSeconds -= 5;
 			else { timerRemainingSeconds = 0; }
 			timerSecondsChanged = true;
 			break;
-		case 2 : // less delay
+		case 2 : // more delay
 			if( timerSeconds < 3595 ) {timerSeconds += 5; }
 			timerRemainingSeconds += 5;
 			timerSecondsChanged = true;
@@ -171,28 +137,28 @@ static void processButton(uint8_t bc)
 
 void loop()
 {
-	//updateTimerLCD();
-	/*
-	for(uint8_t i=0;i<95;i++)
+	uint8_t bc = 0;
+
+	ts.exec( 100000 , [](){ updateTimerLCD(); } ); // 100 mS
+	ts.loop( 10000, [](int32_t){ updateKeyPressed(); } ); // 10 mS
+	for(uint8_t i=0;i<88;i++) // 880 mS
 	{
-		uint8_t bc = 0;
-		rawSerialIO.ts.execFast( 10000, [&bc](){ bc = getButtonCode(); });
-		if(bc!=0)
+		bc = readKeyPressed();
+		if( bc != 0 )
 		{
-			processButton(bc);
-			rawSerialIO.ts.reset();
-			return;
+		    ts.exec( 10000 , [&bc](){ processButton(bc); bc=0; } );
+		    if( i < 78 )
+		    {
+			ts.exec( 100000 , [](){ updateTimerLCD(); } );
+			i += 10;
+		    }
+		}
+		else
+		{
+		    ts.loop( 10000 , [](int32_t){ updateKeyPressed(); } );
 		}
 	}
-	*/
-	ts.exec( 1000000UL, []()
-		{
-			-- timerRemainingSeconds;
-			if( timerRemainingSeconds <= 0 )
-			{
-				powerOff();
-			}
-		});
-
+	
+	ts.exec( 10000, []() { if( (--timerRemainingSeconds) <= 0 ) { powerOff(); } } ); // 10 mS
 }
 
