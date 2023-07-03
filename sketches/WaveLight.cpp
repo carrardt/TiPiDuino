a512cc73b8867cceeca65e1765b2656b95c7f949
Thierry Carrard <thierry.carrard@orange.fr>
2023-07-03 10:17:35 +0200

    Create WaveLight.cpp
    added first skeleton for athletics stadium wave light

---
+++ sketches/WaveLight.cpp
@@ -0,0 +1,28 @@
+#include <AvrTL.h>
+#include <AvrTLPin.h>
+
+#include "BasicIO/PrintStream.h"
+#include "BasicIO/InputStream.h"
+
+#include <avr/interrupt.h>
+
+#define SERIAL_SPEED 19200
+
+#include "HWSerialNoInt/HWSerialNoInt.h"
+ByteStreamAdapter<HWSerialNoInt,10000UL> serialIO;
+PrintStream cout;
+int n = 0;
+
+void setup()
+{
+	cli();
+	serialIO.m_rawIO.begin(SERIAL_SPEED);
+	cout.begin( &serialIO );
+}
+
+
+void loop()
+{
+	n = ( n + 1 ) % 1000;
+	cout << n << '\n';
+}
