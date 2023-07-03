6d4d8f4abe3a983fc788a5deae858f00bab2ba35
Thierry Carrard <thierry.carrard@cea.fr>
2023-07-03 11:18:25 +0200

    added precise timer test

--- sketches/WaveLight.cpp
+++ sketches/WaveLight.cpp
@@ -7,11 +7,15 @@
 #include <avr/interrupt.h>
 
 #define SERIAL_SPEED 19200
-
 #include "HWSerialNoInt/HWSerialNoInt.h"
+
 ByteStreamAdapter<HWSerialNoInt,10000UL> serialIO;
 PrintStream cout;
 int n = 0;
+
+avrtl::AvrTimer0NoPrescaler g_hires_timer;
+
+auto ws2811_pin = avrtl::StaticPin<9>();
 
 void setup()
 {
@@ -23,6 +27,12 @@ void setup()
 
 void loop()
 {
-	n = ( n + 1 ) % 1000;
-	cout << n << '\n';
+  ws2811_pin.Set( false );
+  auto T0 = g_hires_timer.m_timerhw.counter();
+  auto T1 = g_hires_timer.m_timerhw.counter();
+  ws2811_pin.Set( true );
+  auto T2 = g_hires_timer.m_timerhw.counter();
+
+	n = ( n + 1 ) % 100;
+	cout << n << ", E="<<(T1-T0)<<", S="<<(T2-T1)<< '\n';
 }
