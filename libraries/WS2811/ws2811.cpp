ed3bc5cc418d0499f85d33d1bbe697dea37e3597
Thierry Carrard <thierry.carrard@cea.fr>
2023-07-05 16:27:53 +0200

    new WS2811 signal encoding routine

---
+++ libraries/WS2811/ws2811.cpp
@@ -0,0 +1,105 @@
+#include <AvrTL.h>
+#include <AvrTLPin.h>
+
+avrtl::StaticPin<8> ws2811_pin(); // pin 8 => PB0 on atmega328p
+
+void ws2811_send_bytes_PB0( const uint8_t* s, uint16_t len)
+{
+  const uint8_t zero = PORTB & 0xFE ;
+  //const uint8_t one =  zero | 0x01;
+  const uint8_t* end = buffer+len;
+  uint8_t byte = 0;
+  uint8_t bit = 0;
+
+  PORTB = zero;
+
+  delayMicroseconds( 50 );
+
+  do
+  {
+    // bit 0
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+    // bit 1
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+    // bit 2
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+#if 0
+    // bit 3
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+    // bit 4
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+    // bit 5
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+
+    // bit 6
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    byte = byte >> 1;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    // wait to reach 1.3 µs period
+#endif
+
+    // bit 7
+    PORTB = one;
+    bit = zero | ( byte & 0x01 );
+    // wait to reach 0.5 µs period
+    PORTB = bit;
+    // wait to reach 0.7 µs period
+    PORTB = zero;
+    byte = *s++ ;
+    // wait to reach 1.3 µs period
+  } while( s != end );
+
+
+}
+
--- sketches/WaveLight.cpp
+++ sketches/WaveLight.cpp
@@ -4,6 +4,8 @@
 #include "BasicIO/PrintStream.h"
 #include "BasicIO/InputStream.h"
 
+#include "WS2811/ws2811.h"
+
 #include <avr/interrupt.h>
 
 #define SERIAL_SPEED 19200
@@ -22,129 +24,28 @@
  * It means we have to write 1, then the bit to encode and finaly 0 to output port.
  * In between writes we have a little time to perform simple operations (i.e. fetch next bit)
  *
+ * possible LEDs input current setup : R = (Vs - Vled) / Iled
+ * Test model is 10W aRGB, input is DC12V , datasheet says I=300mA for R,G,B , U=6-6.6v for RED , U=9-9.6v for GREEN and BLUE
+ * Rr = (12-6.3)/0.3 = 19 Ohm
+ * Rg = Rb = (12-9.3)/0.3 = 9 Ohm
  */
 
 ByteStreamAdapter<HWSerialNoInt,10000UL> serialIO;
 PrintStream cout;
 avrtl::AvrTimer0NoPrescaler g_hires_timer;
-auto ws2811_pin = avrtl::StaticPin<8>(); // pin 8 => PB0 on atmega328p
 
 void setup()
 {
 	cli();
 	serialIO.m_rawIO.begin(SERIAL_SPEED);
 	cout.begin( &serialIO );
+
 	ws2811_pin.SetOutput();
 }
 
-void ws2811_send_bytes_PB0( const uint8_t* s, uint16_t len)
-{
-  const uint8_t zero = PORTB & 0xFE ;
-  const uint8_t one =  zero | 0x01;
-  const uint8_t* end = buffer+len;
-  uint8_t byte = 0;
-  uint8_t bit = 0;
-
-  PORTB = zero;
-
-  delayMicroseconds( 50 );
-
-  do
-  {
-    // bit 0
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 1
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 2
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 3
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 4
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 5
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 6
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    byte = byte >> 1;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    // wait to reach 1.3 µs period
-
-    // bit 7
-    PORTB = one;
-    bit = zero | ( byte & 0x01 );
-    // wait to reach 0.5 µs period
-    PORTB = bit;
-    // wait to reach 0.7 µs period
-    PORTB = zero;
-    byte = *s++ ;
-    // wait to reach 1.3 µs period
-  } while( s != end );
-
-
-}
-
-
 void loop()
 {
-  ws2811_pin.Set( false );
-  auto T0 = g_hires_timer.m_timerhw.counter();
-  auto T1 = g_hires_timer.m_timerhw.counter();
-  ws2811_pin.Set( true );
-  auto T2 = g_hires_timer.m_timerhw.counter();
-
-	cout << n << ", E="<<(T1-T0)<<", S="<<(T2-T1)<< '\n';
+  static const uint8_t test_buffer[] = { 0x05 , 0x0A , 0x05 };
+  ws2811_send_bytes_PB0( test_buffer , 3 );
 }
-
 
