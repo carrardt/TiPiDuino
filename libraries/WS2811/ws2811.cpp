db09c3e8f3cba08b0bf4da19438e0294f660b28f
Thierry Carrard <thierry.carrard@cea.fr>
2023-07-06 16:24:30 +0200

    comment update

--- libraries/WS2811/ws2811.cpp
+++ libraries/WS2811/ws2811.cpp
@@ -2,6 +2,25 @@
 #include <AvrTLPin.h>
 #include <timer.h>
 
+/*
+ * WS2811 signal structure
+ *          __
+ * bit 0 : |   |________|  : High = 0.5 µs , Low = 2 µs , total= 2.5 µs
+ *          _______
+ * bit 1 : |       |____|  : High = 1.2 µs , Low = 1.3 µs , toal = 2.5 µs
+ *
+ * if we split the 2 signals in 3 phase, we have :
+ * 0.5 µs High , 0.7 µs High/Low depending on bit to encode (Low for 0, High for 1) , 1.3 µs Low
+ *
+ * It means we have to write 1, then the bit to encode and finaly 0 to output port.
+ * In between writes we have a little time to perform simple operations (i.e. fetch next bit)
+ *
+ * possible LEDs input current setup : R = (Vs - Vled) / Iled
+ * Test model is 10W aRGB, input is DC12V , datasheet says I=300mA for R,G,B , U=6-6.6v for RED , U=9-9.6v for GREEN and BLUE
+ * Rr = (12-6.3)/0.3 = 19 Ohm
+ * Rg = Rb = (12-9.3)/0.3 = 9 Ohm
+ */
+
 avrtl::StaticPin<8> ws2811_pin(); // pin 8 => PB0 on atmega328p
 
 void ws2811_send_bytes_PB0( const uint8_t* s, uint16_t len)
--- sketches/WaveLight.cpp
+++ sketches/WaveLight.cpp
@@ -11,26 +11,6 @@
 
 #include <avr/interrupt.h>
 
-
-/*
- * WS2811 signal structure
- *          __
- * bit 0 :    |________ : High = 0.5 µs , Low = 2 µs , total= 2.5 µs
- *         _____
- * bit 1 :      |______ : High = 1.2 µs , Low = 1.3 µs , toal = 2.5 µs
- *
- * if we split the 2 signals in 3 phase, we have :
- * 0.5 µs High , 0.7 µs High/Low depending on bit to encode (Low for 0, High for 1) , 1.3 µs Low
- *
- * It means we have to write 1, then the bit to encode and finaly 0 to output port.
- * In between writes we have a little time to perform simple operations (i.e. fetch next bit)
- *
- * possible LEDs input current setup : R = (Vs - Vled) / Iled
- * Test model is 10W aRGB, input is DC12V , datasheet says I=300mA for R,G,B , U=6-6.6v for RED , U=9-9.6v for GREEN and BLUE
- * Rr = (12-6.3)/0.3 = 19 Ohm
- * Rg = Rb = (12-9.3)/0.3 = 9 Ohm
- */
-
 // PCD8544 pins : SCLK, SDIN, DC, RST, SCE
 #define LCD_PINS     2,    3,  4,   5, PCD8544_UNASSIGNED
 static PCD8544 lcd( LCD_PINS );
