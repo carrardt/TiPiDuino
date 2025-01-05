282954324d431dea650dd3eb6465812d74238317
Thierry Carrard <thierry.carrard@orange.fr>
2025-01-05 17:04:06 +0100

    use functions for initialization steps

--- esp32/micropython/boot.py
+++ esp32/micropython/boot.py
@@ -1,15 +1,17 @@
 #$https://raw.githubusercontent.com/carrardt/TiPiDuino/refs/heads/main/esp32/micropython/boot.py
-#<boot.py
 
 print('-- Import modules --')
-import socket
-import network
+
 import esp
 esp.osdebug(None)
-import machine
-import requests
 import gc
+gc.enable()
+import network
+import machine
+from requests import get
+sta_if = network.WLAN(network.STA_IF)
 gc.collect()
+print('Mem: used=%d , free=%d' % (gc.mem_alloc(),gc.mem_free()) )
 
 print('-- Define functions --')
 def mkfile():
@@ -40,48 +42,45 @@ def chkfile(fname):
     ln=ln+1
 
 # Wifi connection
-print('-- Wifi connection --')
-WIFICON = [ s.strip() for s in open('wifi.txt').readlines() ]
-ssid = WIFICON[0]
-password = WIFICON[1]
-hostname = WIFICON[2]
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-print(sta_if.scan())
-print('Configure hostname to %s' % hostname)
-sta_if.config(dhcp_hostname=hostname)
-print('Connecting to %s with key %s' % (ssid,password) )
-sta_if.connect(ssid,password)
-
-# wait for connection established
-conwait=0
-print('Waiting for connection ',end="")
-while not sta_if.isconnected():
-  machine.sleep(1000)
-  conwait=conwait+1
-  print('.',end='')
-
-print('\nConnection successful')
-print(sta_if.ifconfig())
+def wifi_connect():
+  print('-- Wifi connection --')
+  WIFICON = [ s.strip() for s in open('wifi.txt').readlines() ]
+  ssid = WIFICON[0]
+  password = WIFICON[1]
+  hostname = WIFICON[2]
+  sta_if.active(True)
+  print(sta_if.scan())
+  print('Configure hostname to %s' % hostname)
+  sta_if.config(dhcp_hostname=hostname)
+  print('Connecting to %s with key %s' % (ssid,password) )
+  sta_if.connect(ssid,password)
+  machine.sleep(5000)
+  print(sta_if.ifconfig())
+  return sta_if.isconnected()
 
 # Check / update wan IP address
-print('-- Setup wan interface --')
-try:
-  WANIP = [ s.strip() for s in open('wanip.txt').readlines() ]
-  print('last configured wan IP for %s was %s' % (WANIP[0],WANIP[1]) )
-  curwanip = requests.get('https://api.ipify.org').content.decode('utf8')
-  print('current wan IP is %s' % curwanip )
-  if curwanip==WANIP[1]:
-    print('wan IP is unchanged, no update')
-  else:
-    print('update wan IP to %s' % curwanip)
-    WANIP[1]=curwanip
-    print(requests.get(WANIP[2]).content.decode('utf8'))
-    f=open('wanip.txt','w')
-    f.write('\n'.join(WANIP))
-    f.close()
-except:
-  print('no dynamic wan IP configuration found')
+def setup_wanip():
+  print('-- Setup wan interface --')
+  try:
+    WANIP = [ s.strip() for s in open('wanip.txt').readlines() ]
+    print('last configured wan IP for %s was %s' % (WANIP[0],WANIP[1]) )
+    curwanip = get('https://api.ipify.org').content.decode('utf8')
+    print('current wan IP is %s' % curwanip )
+    if curwanip==WANIP[1]:
+      print('wan IP is unchanged, no update')
+    else:
+      print('update wan IP to %s' % curwanip)
+      WANIP[1]=curwanip
+      print(get(WANIP[2]).content.decode('utf8'))
+      f=open('wanip.txt','w')
+      f.write('\n'.join(WANIP))
+      f.close()
+  except:
+    print('no dynamic wan IP configuration found')
 
 gc.collect()
+print('Mem: used=%d , free=%d' % (gc.mem_alloc(),gc.mem_free()) )
 
+wifi_connect()
+machine.sleep(3000)
+setup_wanip()
