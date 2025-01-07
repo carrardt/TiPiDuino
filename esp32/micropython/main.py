7be7c4b3c93d0e375fdf6b7010607031bf650394
Thierry Carrard <thierry.carrard@cea.fr>
2025-01-07 14:39:55 +0100

    reworking web response server

--- esp32/micropython/main.py
+++ esp32/micropython/main.py
@@ -42,7 +42,8 @@ while True:
   else:
     gpio_state="OFF"
   conn.send('HTTP/1.1 200 OK\n')
-  conn.send('Content-Type: text/html\n')
+  conn.send('Content-Type: text/html\n') # use text/javascript for .js files
+  #conn.send('Cache-Control: public, max-age=604800\n')
   conn.send('Connection: close\n\n')
   conn.sendall( open('index.html').read() % gpio_state )
   conn.close()
