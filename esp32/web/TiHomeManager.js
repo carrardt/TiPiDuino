25d28a3335d19b562f6d4d67bf759369fed831b3
Thierry Carrard <thierry.carrard@cea.fr>
2025-01-09 16:20:50 +0100

    changed colors en centering

--- esp32/web/TiHomeManager.js
+++ esp32/web/TiHomeManager.js
@@ -1,3 +1,19 @@
+function heatStateColor(m)
+{
+  let color = "white";
+  if(m=="C") color="pink";
+  else if(m=="E") color="lightgrey";
+  else if(m=="F") color="lightblue";          
+//  else if(m=="O") color="white";
+  return color;
+}
+
+function heatStateSwitch(home,z,d,h,q)
+{
+  let m = home.heatzones[z].schedule[d][h*4+q]
+  return color;
+}
+
 function heatZoneTable(home)
 {
   const daystr=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"];
@@ -13,14 +29,18 @@ function heatZoneTable(home)
     td = tr.insertCell();
     td.appendChild(document.createTextNode(h.toString().padStart(2,"0")+"h"));
     td.style.border = '1px solid black';
+    td.style["text-align"] = 'center';
     td.setAttribute('colSpan', '4');
   }
   for (let z = 0; z < nzones; z++)
   {
     tr = tbl.insertRow();
     td = tr.insertCell();
-    td.appendChild(document.createTextNode(`Zone ${z} ${home.heatzones[z].state}`));
+    td.appendChild(document.createTextNode(`Zone ${z}`));
     td.style.border = '1px solid black';
+    let color = heatStateColor( home.heatzones[z].state );
+    //b.style.color = color;
+    td.style["background-color"] = color;
     for (let d = 0; d < 7; d++)
     {
       tr = tbl.insertRow();
@@ -33,15 +53,13 @@ function heatZoneTable(home)
         {
           td = tr.insertCell();
           let b = document.createElement('button');
-          let color = "black";
-          let m =  home.heatzones[z].schedule[d][h*4+q];
-          if(m=="C") color="green";
-          else if(m=="E") color="orange";
-          else if(m=="F") color="red";          
-          else if(m=="O") color="black";          
+          let color = heatStateColor( home.heatzones[z].schedule[d][h*4+q] );
+          b.id = `z${z}d${d}h${h}q${q}`;
           b.innerHTML = "X";
+          b.style.border = 0;
           b.style.color = color;
           b.style["background-color"] = color;
+          b.onclick = '';
           td.appendChild(b);
         }
         td.style.border = 0;
