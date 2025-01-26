f114c955b638fa778b7415a84cd808e229cbdf2b
Thierry Carrard <thierry.carrard@orange.fr>
2025-01-26 17:25:13 +0100

    implementing DB query interface for JS clients

--- esp32/micropython/console.py
+++ esp32/micropython/console.py
@@ -2,6 +2,8 @@ import machine
 
 # console output to PCD8544 LCD screen
 import pcd8544
+# SPI #1 means pins 13 (MOSI) and 14 (SCLK) for ESP32 WROOM 32
+# 3 other pins are CS, DC and RST respectively
 lcd = pcd8544.PCD8544_BM8x5(machine.SPI(1), machine.Pin(12), machine.Pin(15), machine.Pin(4))
 lcd.reset()
 lcd.init()
