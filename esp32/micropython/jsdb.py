5d99942ac3bea2a9573695e0a2b94d9c7c4f12ad
Thierry Carrard <thierry.carrard@orange.fr>
2025-02-01 20:24:52 +0100

    partial object query/update implemented

--- esp32/micropython/jsdb.py
+++ esp32/micropython/jsdb.py
@@ -1,7 +1,3 @@
-def jsdb_init():
-  if not 'jsdb' in globals():
-    globals()['jsdb'] = {}
-
 def jsdb_parse_key(k):
   objpath = k.split('.')
   objroot = objpath[0]
@@ -9,18 +5,22 @@ def jsdb_parse_key(k):
   for o in objpath:
     a = o.split('[')
     objacc += "[".join(["['"+a[0]+"']"]+a[1:])
+  if not 'jsdb' in globals():
+    globals()['jsdb'] = {}
+  if not objroot in globals()['jsdb']:
+    exec( ("jsdb['%s']="%k) + open('web/%s.json'%k).read() + '\n' , globals() )
   return (objroot,objacc)
 
 def jsdb_get_value(k):
-  jsdb_init()
-  if not k in globals()['jsdb']
-    exec( ("jsdb['%s']="%k) + open('web/%s.json'%k).read() + '\n' , globals() )
-  return globals()['jsdb'][k]
+  (objroot,objacc) = jsdb_parse_key(k)
+  L={}
+  exec('R=jsdb'+objacc,globals(),L)
+  return L['R']
 
 def jsdb_set_value(k,v):
-  jsdb_init()
-  jsdb_get_value(k)
-  globals()['jsdb'][k] = v
+  (objroot,objacc) = jsdb_parse_key(k)
+  L={'V':v}
+  exec('jsdb'+objacc+'=V',globals(),L)
   return True
 
 def db_query(conn,fname,mimetype,params):
@@ -29,11 +29,13 @@ def db_query(conn,fname,mimetype,params):
     http_reply_header(conn,"application/json",False)
   else:
     dmesg('EFMT:%s'%fmt)
+    conn.sendall('EFMT:%s\n'%fmt)
     return False
   for (k,v) in params.items():
     if v=='=':
       conn.sendall('let %s = %s ;\n'%(k,jsdb_get_value(k)))
     else:
       jsdb_set_value(k,v)
+  conn.sendall('let jsdb_errcode = 0;\n')
   return True
 
