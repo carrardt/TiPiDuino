5e2475206c973d46b9d0156bae16a8f0cc943c06
Thierry Carrard <thierry.carrard@orange.fr>
2025-02-17 20:42:21 +0100

    separate directory for database objects

--- esp32/micropython/setup.py
+++ esp32/micropython/setup.py
@@ -3,8 +3,6 @@
 import os
 import requests
 
-os.mkdir('config')
-os.mkdir('web')
 
 def download_py(fname):
   bname=fname.split('/')[-1]
@@ -16,12 +14,29 @@ def download_py(fname):
 for fname in ['boot.py','console.py','http_server.py','main.py','tools.py','wanip.py','wifi.py','jsdb.py','pcd8544/pcd8544.py']:
   download_py(fname)
 
+
+
+os.mkdir('web')
+
 def download_web(webfile):
   print("Download %s ..."%webfile)
   f=open("web/%s"%webfile,'w')
   f.write(requests.get("https://raw.githubusercontent.com/carrardt/TiPiDuino/refs/heads/main/esp32/web/%s"%webfile).content.decode('utf8'))
   f.close()
 
-for webfile in ['index.html','setup-wifi.html','setup-wan.html','TiHomeManager.html','TiHomeManager.js','home.json','wifi.json','wanip.json']:
+for webfile in ['index.html','setup-wifi.html','setup-wan.html','TiHomeManager.html','TiHomeManager.js']:
   download_web(webfile)
+
+
+
+os.mkdir('db')
+
+def download_db(dbfile):
+  print("Download %s ..."%dbfile)
+  f=open("db/%s"%dbfile,'w')
+  f.write(requests.get("https://raw.githubusercontent.com/carrardt/TiPiDuino/refs/heads/main/esp32/db/%s"%webfile).content.decode('utf8'))
+  f.close()
+
+for dbfile in ['home.json','wifi.json','wanip.json']:
+  download_db(dbfile)
 
